9e661783aba739028c102123e1e3a22e9bacdd02
Sam Nosenzo <samnosenzo@gmail.com>
2023-08-29 14:42:35 -0400

    - don't export referencetypeidlnode

--- packages/omgidl-parser/src/IdlNodes/index.ts
+++ packages/omgidl-parser/src/IdlNodes/index.ts
@@ -2,7 +2,6 @@ export * from "./ConstantIdlNode";
 export * from "./EnumIdlNode";
 export * from "./IdlNode";
 export * from "./ModuleIdlNode";
-export * from "./ReferenceTypeIdlNode";
 export * from "./StructIdlNode";
 export * from "./StructMemberIdlNode";
 export * from "./TypedefIdlNode";
